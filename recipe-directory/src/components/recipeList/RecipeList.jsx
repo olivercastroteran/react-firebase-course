3c78b805f1d4ec82101867ce3d4905d7f4e05659
oliver <olivercastroteran@gmail.com>
2022-05-18 23:06:01 -0400

    rendering RecipesList and individual recipe on UI

--- recipe-directory/src/components/recipeList/RecipeList.jsx
+++ recipe-directory/src/components/recipeList/RecipeList.jsx
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom';
 import './RecipeList.css';
 
-const RecipeList = () => {
+const RecipeList = ({ recipes }) => {
   return (
     <div className="recipe-list">
-      <div className="card"></div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="card">
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to make.</p>
+          <div>{recipe.method.substring(0, 100)}...</div>
+          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+        </div>
+      ))}
     </div>
   );
 };
--- recipe-directory/src/views/home/Home.jsx
+++ recipe-directory/src/views/home/Home.jsx
@@ -3,17 +3,13 @@ import { useFetch } from '../../hooks/useFetch';
 import './Home.css';
 
 const Home = () => {
-  const {
-    data: recipes,
-    isPending,
-    error,
-  } = useFetch('http://localhost:3000/recipes');
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
 
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {recipes && <RecipeList {...recipes} />}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 };
