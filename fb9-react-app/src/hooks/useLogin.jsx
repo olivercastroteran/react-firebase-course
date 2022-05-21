5218d3793ec00030e3cd77011bf987a385e20725
oliver <olivercastroteran@gmail.com>
2022-05-21 15:25:15 -0400

    AuthContext created and auth logic implemented

--- fb9-react-app/src/hooks/useLogin.jsx
+++ fb9-react-app/src/hooks/useLogin.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
+import { useAuthContext } from '../hooks/useAuthContext';
 // firebase imports
 import { auth } from '../firebase/config';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export const useLogin = () => {
   const [error, setError] = useState(null);
+  const { dispatch } = useAuthContext();
 
   const login = (email, password) => {
     setError(null);
 
     signInWithEmailAndPassword(auth, email, password)
       .then((res) => {
-        console.log('user logged in: ', res.user);
+        dispatch({ type: 'LOGIN', payload: res.user });
       })
       .catch((err) => {
         setError(err.message);
